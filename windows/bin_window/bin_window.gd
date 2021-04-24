5772b6f66b6e83d8b40c660243c2c61ecdc80fa9
NoahJacobsen <noahedjacobsen@gmail.com>
2021-04-24 14:01:17 -0700

    Menu rotation works

--- windows/bin_window/bin_window.gd
+++ windows/bin_window/bin_window.gd
@@ -1,34 +1,56 @@
 extends Control
 
-const MAX_PROGRAMS = 15
+const MAX_PROGRAMS = 5
 
 onready var program_button_res = load("res://windows/bin_window/program_button.tscn")
 onready var program_res = load("res://programs/PROGRAM.gd")
 
 var program_list = []
-#	{"ID": "1", "color": Color.cyan, "name": "pwDICT_roller"},
-#	{"ID": "2", "color": Color.red, "name": "LZW_decrypt"}
 
 func _ready():
 	#update_list()
 	$Main/NewButton.connect("button_down", self, "new_button")
+	$EditCosmetics/NextButton.connect("button_down", self, "next_button")
+	$EditStats/DoneButton.connect("button_down", self, "done_button")
+	transition_to("main")
 
 func update_list():
 	for child in $Main/ProgramList.get_children():
 		$Main/ProgramList.remove_child(child)
 	for program in program_list:
 		var entry = program_button_res.instance()
-		entry.set_appearance(program["color"], program["name"])
+		entry.set_appearance(program.color, "New")
 		$Main/ProgramList.add_child(entry)
 
+func update_editors(p):
+	pass
+
 func new_button():
-	print("Click!")
 	if program_list.size() >= MAX_PROGRAMS:
 		print("You have reached the maximum number of programs.")
 		return
-	program_list.append({
-		"ID": "0",
-		"color": Color.cyan,
-		"name": "new"
-	})
+	var p = program_res.new()
+	#Add program to list and begin editing it
+	program_list.append(p)
+	update_editors(p)
+	transition_to("cosmetics")
+
+func next_button():
+	transition_to("stats")
+
+func done_button():
 	update_list()
+	transition_to("main")
+
+func transition_to(menu):
+	if (menu == "main"):
+		$EditStats.visible = false
+		$Main.visible = true
+	elif (menu == "cosmetics"):
+		$Main.visible = false
+		$EditCosmetics.visible = true
+	elif (menu == "stats"):
+		$EditCosmetics.visible = false
+		$EditStats.visible = true
+	else:
+		print("ERROR - menu not found:", menu)
