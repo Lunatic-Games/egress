3f2c6f7db551b37a5ecbd6ce3bdc97f7ad33b33b
NoahJacobsen <noahedjacobsen@gmail.com>
2021-04-24 13:40:21 -0700

    Simple new button

--- windows/bin_window/bin_window.gd
+++ windows/bin_window/bin_window.gd
@@ -1,28 +1,34 @@
 extends Control
 
+const MAX_PROGRAMS = 15
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var program_button_res = load("res://windows/bin_window/program_button.tscn")
+onready var program_res = load("res://programs/PROGRAM.gd")
 
+var program_list = []
+#	{"ID": "1", "color": Color.cyan, "name": "pwDICT_roller"},
+#	{"ID": "2", "color": Color.red, "name": "LZW_decrypt"}
 
-const program_resource = preload("res://windows/bin_window/program_button.tscn")
-
-var programs = [
-	{"ID": "1", "color": Color.cyan, "name": "pwDICT_roller"},
-	{"ID": "2", "color": Color.red, "name": "LZW_decrypt"}
-	]
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	update_list()
+	#update_list()
+	$Main/NewButton.connect("button_down", self, "new_button")
 
 func update_list():
-	for program in programs:
-		var entry = program_resource.instance()
+	for child in $Main/ProgramList.get_children():
+		$Main/ProgramList.remove_child(child)
+	for program in program_list:
+		var entry = program_button_res.instance()
 		entry.set_appearance(program["color"], program["name"])
-		$ProgramList.add_child(entry)
+		$Main/ProgramList.add_child(entry)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func new_button():
+	print("Click!")
+	if program_list.size() >= MAX_PROGRAMS:
+		print("You have reached the maximum number of programs.")
+		return
+	program_list.append({
+		"ID": "0",
+		"color": Color.cyan,
+		"name": "new"
+	})
+	update_list()
