a42dbfed14a9424559381ace60514ccab5e34e44
Matthias Harden <43286653+SirDuck145@users.noreply.github.com>
2021-04-25 18:00:46 -0700

    Add signals for Noah (#19)

--- hacker/hacker.gd
+++ hacker/hacker.gd
@@ -17,10 +17,17 @@ onready var strong_against = {
 onready var trace = 0.0
 onready var max_trace = 100.0
 onready var bits = 0
+onready var total_points = 0
+onready var bits_per_point = 100
+onready var bits_segment = 0
 
+
+signal gained_point
 
 func gain_bits(bits):
 	self.bits += bits
+	self.bits_segment += bits
+	set_points()
 
 
 func gain_trace(trace):
@@ -28,6 +35,13 @@ func gain_trace(trace):
 	print("Trace at ", self.trace, "%")
 	if (self.trace >= max_trace):
 		print("GAME OVER")
+
+
+func set_points():
+	while(bits_segment >= 100):
+		total_points += 1
+		bits_segment -= 100
+		emit_signal("gained_point")
 
 
 func weak_against(type):
